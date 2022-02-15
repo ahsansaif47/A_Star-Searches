cd3f34776b6d2b3774b1c43e9de84594b7534a13
ahsansaif47 <ahsan_saif47@hotmail.com>
2022-02-15 11:50:31 +0500

    Single Agent A*

---
+++ Single Agent AStar.py
@@ -0,0 +1,24 @@
+import queue
+
+
+def createMaze():
+    maze = []
+    maze.append(["#", "#", "#", "#", "#", "O", "#"])
+    maze.append(["#", " ", " ", " ", "#", " ", "#"])
+    maze.append(["#", " ", "#", " ", "#", " ", "#"])
+    maze.append(["#", " ", "#", " ", " ", " ", "#"])
+    maze.append(["#", " ", "#", "#", "#", " ", "#"])
+    maze.append(["#", " ", " ", " ", "#", " ", "#"])
+    maze.append(["#", "#", "#", "#", "#", "X", "#"])
+
+    return maze
+
+
+Maze = createMaze()
+
+
+def printMaze():
+    for i in range(len(Maze)):
+        for j in range(len(Maze)):
+            print(Maze[i][j], " ", end="")
+        print()
