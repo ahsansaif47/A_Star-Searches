d9589b270f7ec64aeb1f411c49c7d2d7a0806421
ahsansaif47 <ahsan_saif47@hotmail.com>
2022-02-15 13:26:26 +0500

    single agent movement changes

--- Single Agent AStar.py
+++ Single Agent AStar.py
@@ -1,3 +1,4 @@
+from asyncio.windows_events import NULL
 import queue
 
 
@@ -14,11 +15,25 @@ def createMaze():
     return maze
 
 
-Maze = createMaze()
-
-
-def printMaze():
+def printMaze(Maze):
     for i in range(len(Maze)):
         for j in range(len(Maze)):
             print(Maze[i][j], " ", end="")
         print()
+
+
+def checkMovements(Maze, r, c):
+    valMovements = []
+    if r-1 >= 0 and Maze[r-1][c] != "#":
+        valMovements.append('U')
+    if r+1 < len(Maze) and Maze[r+1][c] != "#":
+        valMovements.append('D')
+    if c-1 >= 0 and Maze[r][c-1] != "#":
+        valMovements.append('L')
+    if c+1 < len(Maze) and Maze[r][c+1] != "#":
+        valMovements.append('R')
+
+    return valMovements
+
+
+Maze = createMaze()
